16f8273e38f5782938911b2dc6e98cc46557bcfd
JamesShillah <jshillah7@gmail.com>
2025-06-24 14:00:14 +0300

    Add files via upload

---
+++ src_index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DerivTradingPlatform from './DerivTradingPlatform';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DerivTradingPlatform />
+  </React.StrictMode>
+);
